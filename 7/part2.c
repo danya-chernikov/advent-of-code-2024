2aa1cee2589d6cd1ef3a455363d13aed1e305eee
danya-chernikov <dchernik@proton.me>
2024-12-08 02:19:46 +0100

    The algoritms of the second part of the seventh day puzzle was corrected

--- 7/part2.c
+++ 7/part2.c
@@ -1,9 +1,12 @@
+
 #include "libft.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
+
+# define _OPEN_SYS_ITOA_EXT
 
 /* MAX_NUMS_STR    - the maximum number of
  *					 numbers in a string;
@@ -99,12 +102,28 @@ t_ll	count_digits(t_ll num)
 	return (dnum);
 }
 
-t_ll	join_numbers(t_ll num1, t_ll num2)
+/*t_ll	join_numbers(t_ll num1, t_ll num2)
 {
 	t_ll	res;
 	char	num1_buf[MAX];
 
 
+	return (res);
+}*/
+
+t_ll	join_nums(t_ll num1, t_ll num2)
+{
+	t_ll	res;
+	char	bnum1[MAX_NUM_STR_LEN + 1];
+	char	bnum2[MAX_NUM_STR_LEN + 1];
+	char	bret[MAX_NUM_STR_LEN + 1];
+
+	snprintf(bnum1, MAX_NUM_STR_LEN, "%lld", num1);
+	snprintf(bnum2, MAX_NUM_STR_LEN, "%lld", num2);
+	ft_strlcpy(bret, bnum1, MAX_NUM_STR_LEN);
+	ft_strlcat(bret, bnum2, MAX_NUM_STR_LEN);
+	
+	res = ft_atoll(bret);
 	return (res);
 }
 
@@ -117,25 +136,21 @@ int	main(int argc, char *argv[])
 	/* Number of lines read from a file */
 	int		line_cnt;
 
-	/* The length of the last
-	 * line read from a file */
-	int		line_len; 
 
 	int		ch;
 	int		i;
 	FILE	*fptr;
 
 	char	ops[MAX_NUMS_STR + 1];	/* Operators */
-	t_ll	eq[MAX_NUMS_STR];		/* Current equation */
 	int		nums_found;				/* Number of numbers found in an equation */
+	t_ll	eq[MAX_NUMS_STR];		/* Current equation */
 
 	/* Number of positions (spaces)
 	 * between numbers in an equation */
 	int		pos_num;
 
-	int		add_flag;
+	//int		add_flag;
 	int		perms_num;				/* Permutations number */
-	//t_ll	op_res;					/* Result of an operation */
 	t_ll	total_calib_res;		/* Total calibration result */
 
 	if (argc != 2)
@@ -166,7 +181,6 @@ int	main(int argc, char *argv[])
 			if (line_cnt < MAX_LINE_NUM)
 			{
 				line[i] = '\0';
-				line_len = i;
 				i = 0;
 
 				nums_found = get_nums(line, eq, MAX_NUMS_STR);
@@ -182,7 +196,7 @@ int	main(int argc, char *argv[])
 					perms_num = (int)pow((double)2, (double)pos_num);
 					printf("\tperms_num = %d\n", perms_num);
 
-					add_flag = 1;
+					//add_flag = 1;
 					/* Number of bits in permutation is equal the number of positions */
 					for (int perm = 0; perm < perms_num; perm++)
 					{
@@ -197,9 +211,9 @@ int	main(int argc, char *argv[])
 								ops[bit_ind] = '?';
 						}
 
-						/* Let's print */
-						printf("\t");
-						for (int j = 0; j < nums_found; j++)
+						/* Let's print inicial operations */
+						printf("\tops = ");
+						for (int j = 0; j < pos_num; j++)
 							printf("%c", ops[j]);
 						printf("\n");
 
@@ -226,8 +240,11 @@ int	main(int argc, char *argv[])
 							if (ops[pi] == '?')
 								l_pos_num++;
 						}
+						printf("\tl_pos_num = %d\n", l_pos_num);
 
 						int	l_perms_num = (int)pow((double)2, (double)l_pos_num);
+
+						printf("\tl_perms_num = %d\n", l_perms_num);
 
 						for (int l_perm = 0; l_perm < l_perms_num; l_perm++)
 						{
@@ -235,29 +252,45 @@ int	main(int argc, char *argv[])
 							for (int ci = 0; ci < pos_num; ci++)
 								ops_copy[ci] = ops[ci];
 
-							/* Let's generate local l_ops array containing
-							 * only '+' and '*' operators */
-							for (int lbi = 0; lbi < l_pos_num; lbi++)
-							{
-								if (l_perm & (1 << lbi))
-									l_ops[lbi] = '+';
-								else
-									l_ops[lbi] = '*';
-							}
+							printf("\t\tops_copy = ");
+							for (int j = 0; j < pos_num; j++)
+								printf("%c", ops_copy[j]);
+							printf("\n");
 
-							int	l_pos_ind = 0;
-							/* Let's unite ops_copy array with l_pos */
-							for (int q = 0; q < pos_num; q++)
+							/* Let's generate local l_ops array containing
+							 * only '+' and '*' operators. But only if we
+							 * are dealing with an operation array containing
+							 * at least one '?' (which means: put here '+' or '*'
+							 * operators) */
+							if (l_pos_num > 0) // If there were at least one '?'
 							{
-								if (ops_copy[q] == '?')
+								for (int lbi = 0; lbi < l_pos_num; lbi++)
 								{
-									ops_copy[q] = l_ops[l_pos_ind];
-									l_pos_ind++;
+									if (l_perm & (1 << lbi))
+										l_ops[lbi] = '+';
+									else
+										l_ops[lbi] = '*';
+								}
+		
+								printf("\t\tl_ops = ");
+								for (int j = 0; j < l_pos_num; j++)
+									printf("%c", l_ops[j]);
+								printf("\n");
+
+								int	l_pos_ind = 0;
+								/* Let's unite ops_copy array with l_pos */
+								for (int q = 0; q < pos_num; q++)
+								{
+									if (ops_copy[q] == '?')
+									{
+										ops_copy[q] = l_ops[l_pos_ind];
+										l_pos_ind++;
+									}
 								}
 							}
 
-							/* Printing the result */
-							printf("\t\t");
+							/* Printing the generated operation */
+							printf("\t\tops_copy = ");
 							for (int j = 1; j < nums_found; j++)
 							{
 								printf("%lld ", eq[j]);
@@ -268,31 +301,73 @@ int	main(int argc, char *argv[])
 							}
 							printf("= \n");
 
+							/* Partial reults of calculations to concatenate.
+							 * The results of calculations the expressions
+							 * that are located between '|' operators */
+
+							/* Calculate the result of the
+							 * intermediate operation */
+							t_ll	parts[MAX_NUM_STR_LEN];
+
+							/* Result of the
+							 * intermediate operation */
+							t_ll	op_res; 
+
+							int		part_ind;
+							int		j;
+
+							part_ind = 0;
+							op_res = eq[1];
+							printf("\t\tRESULT = ");
+							j = 2;
+							while (j - 1 < pos_num + 2)
+							{
+								if (ops_copy[j - 2] == '|' || j == nums_found)
+								{
+									parts[part_ind] = op_res;
+									printf("%lld|", parts[part_ind]);
+									part_ind++;
+									if (j < nums_found)
+										op_res = eq[j];
+								}
+								if (j < nums_found)
+								{
+									if (ops_copy[j - 2] == '+')
+										op_res += eq[j];
+									if (ops_copy[j - 2] == '*')
+										op_res *= eq[j];
+								}
+								else
+									break;
+
+								j++;
+							}
+							/* Now let's concatenate all the parts */
+							j = 1;
+							op_res = parts[0];
+							while (j < part_ind - 1)
+							{
+								op_res += join_nums(op_res, parts[j]);
+								j++;
+							}
+							printf(" = %lld", op_res);
+
+							printf("\n");
+
+							/*if (op_res == eq[0])
+							{
+								if (add_flag)
+									total_calib_res += op_res;
+								add_flag = 0;
+								printf("\033[32m%lld !!!\033[37m", op_res);
+							}
+							else
+								printf("%lld", op_res);*/
+
 						} // for (int l_perm = 0; l_perm < l_perms_num; l_perm++)
 
 
 						/* ############################################################ */
-
-
-						/* Calculating the operation result */
-						/*op_res = eq[1];
-						for (int j = 2; j < nums_found; j++)
-						{
-							if (ops[j - 2] == '+')
-								op_res += eq[j];
-							else
-								op_res *= eq[j];
-						}
-						if (op_res == eq[0])
-						{
-							if (add_flag)
-								total_calib_res += op_res;
-							add_flag = 0;
-							printf("\033[32m%lld !!!\033[37m", op_res);
-						}
-						else
-							printf("%lld", op_res);
-						*/
 
 						printf("\n");
 					} // for (int perm = 0; perm < perms_num; perm++)
@@ -305,7 +380,6 @@ int	main(int argc, char *argv[])
 			}
 			else
 			{
-				line_len = i;
 				line[i] = '\0';
 				break;
 			}
@@ -314,7 +388,6 @@ int	main(int argc, char *argv[])
 			line[i] = ch;
 		else
 		{
-			line_len = i;
 			line[i] = '\0';
 			break;
 		}
